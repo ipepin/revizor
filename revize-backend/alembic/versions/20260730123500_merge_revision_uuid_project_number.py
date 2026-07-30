0127272549fdadb7e83bff18fb5324eb0489ad6b
pepinoBl <blazek1.jo@gmail.com>
2026-07-30 12:41:44 +0200

    Merge Alembic heads for cloud deploy

---
+++ revize-backend/alembic/versions/20260730123500_merge_revision_uuid_project_number.py
@@ -0,0 +1,24 @@
+"""merge revision uuid and project number heads
+
+Revision ID: 20260730123500_merge_revision_uuid_project_number
+Revises: 20260302195900_add_revision_uuid, 20260302223000_add_project_number
+Create Date: 2026-07-30 12:35:00.000000
+"""
+
+from alembic import op  # noqa: F401
+import sqlalchemy as sa  # noqa: F401
+
+
+revision = "20260730123500_merge_revision_uuid_project_number"
+down_revision = ("20260302195900_add_revision_uuid", "20260302223000_add_project_number")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
+
